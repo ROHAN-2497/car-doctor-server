4e42ea5cbea024776f8dbe700c6dd8eec20af41c
rohan future devoloper <xrohaislam373@gmail.com>
2023-07-24 22:46:23 +0600

    google Authentication Server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -20,6 +21,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ err: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -27,6 +47,16 @@ async function run() {
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollectioin = client.db("carDoctor").collection("booking");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      console.log(token);
+      res.send({ token });
+    });
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -48,8 +78,13 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/booking", async (req, res) => {
-      console.log(req.query.email);
+    app.get("/booking", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("come back after verify", decoded);
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "forbidden access " });
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -73,7 +108,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/bookings/:id", async(req, res) => {
+    app.patch("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
